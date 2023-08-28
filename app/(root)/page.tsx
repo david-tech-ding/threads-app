c59fc337ddf0d91b109b0a60404283921246dfab
David Ding <dingtechxing16@gmail.com>
2023-08-28 12:57:45 +0800

    fetchPosts backend implemented with basic ThreadCard structure

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,10 @@
-//app/page.tsx
-import { UserButton } from "@clerk/nextjs";
+import { fetchPosts } from "@/lib/action/thread.action";
 
-export default function Home() {
+export default async function Home() {
+  const result = await fetchPosts(1, 30);
+
+    console.log(result);
+    
   return (
     <>
       <h1 className="head-text text-left">Home</h1>
--- lib/action/thread.action.ts
+++ lib/action/thread.action.ts
@@ -30,6 +30,31 @@ export async function createThread({text, author, communityId, path}: Params) {
    } catch (error: any) {
     throw new Error(`Error created thread: ${error.message}`)
    }
-   
-    
+}
+
+export async function fetchPosts(pageNumber = 1, pageSize = 20) {
+    connectToDB();
+    const skipAmount = (pageNumber - 1) * pageSize;
+
+    const postsQuery = Thread.find({parentId: {$in : [null, undefined]}})
+    .sort({createdAt: 'desc'})
+    .skip(skipAmount)
+    .limit(pageSize)
+    .populate({path: 'author', model: User})
+    .populate({
+        path: 'children',
+        populate : {
+            path: 'author',
+            model: User,
+            select: "_id name parentId image"
+        }
+    })
+
+    const totalPostsCount = await Thread.countDocuments({parentId: {$in : [null, undefined]}})
+
+    const posts = await postsQuery.exec()
+
+    const isNext = totalPostsCount > skipAmount + posts.length
+
+    return {posts, isNext}
 }
